d2f8e74cfc228e46d79b9a8739240dd5f0c6f6ca
Tarun Kumar <tarunmzn98@gmail.com>
2021-07-16 21:30:28 +0530

    recommendation and movie cast components

--- src/Components/Feedback/FeedbackCard.js
+++ src/Components/Feedback/FeedbackCard.js
@@ -7,15 +7,19 @@ const twitter = ""
 const instagram = ""
 const github = ""
 
-function FeedbackCard() {
+function FeedbackCard(props) {
+  var extraclass = ''
+  if(props.extraclass !==undefined  ) {
+      extraclass = props.extraclass
+  }
   return (
-    <div className="card-container" >
+    <div className={"card-container" + extraclass} >
       <div className="card">
         <div className="card-content">
            <div className="card-heading">
              <h2> This is awesome </h2>
           </div>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias vel eaque iusto magni consequuntur, eius, blanditiis laudantium quod quasi, architecto perferendis. Doloremque minima eos similique incidunt optio deleniti vitae id?</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias vel eaque iusto magni consequuntur,?</p>
           
            <div className="icons">
              <a href={twitter}><i className="fab fa-twitter"></i></a>
