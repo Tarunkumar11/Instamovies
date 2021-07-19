4c57984e817f8c0bd96693ae7d76725e6b81bf43
Tarun Kumar <tarunmzn98@gmail.com>
2021-07-19 15:12:52 +0530

    Added filter

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,11 +1,57 @@
 import React from 'react'
 import './Filter.css'
 
-
 function Filter() {
+    const crossicon = ""
+    function closeFilter(){
+        const filter  =  document.getElementById("filterid")
+        if(filter !== null) {
+            filter.style.display = "none"
+        }
+
+    }
     return (
-        <div>
-            
+        <div className="filter" id="filterid">
+            <div className="close-icon">
+                <img src={crossicon} alt="cross" onClick={closeFilter} />
+            </div>
+            <div className="filter-container">
+                <h2>Filters</h2>
+                <ul>
+                    <li><a href="#">
+                        <select>
+                            <option>genres</option>
+                            <option>a</option>
+                            <option>a</option>
+                            <option>a</option>
+                        </select>
+                    </a></li>
+                    <li><a href="#">
+                        <select>
+                            <option>release year</option>
+                            <option>a</option>
+                            <option>a</option>
+                            <option>a</option>
+                        </select>
+                    </a></li>
+                    <li><a href="#">
+                        <select>
+                            <option>Bollywood</option>
+                            <option>a</option>
+                            <option>a</option>
+                            <option>a</option>
+                        </select>
+                    </a></li>
+                    <li><a href="#">
+                        <select>
+                            <option>Hollywood</option>
+                            <option>a</option>
+                            <option>a</option>
+                            <option>a</option>
+                        </select>
+                    </a></li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/Components/Navbar/NavbarList.js
+++ src/Components/Navbar/NavbarList.js
@@ -2,10 +2,26 @@ import React from 'react'
 
 function NavbarList(props) {
     const navItem = props.items;
+    function filterHandle(e){
+        const filter  =  document.getElementById("filterid")
+        if(filter !== null) {
+            filter.style.display = "inline"
+        }
+        console.log("sdflkjaskl")  
+          
+    }
+    
     return (
         <nav>
             {
-                navItem.map((item) => {return <li key={item}><a href="#">{item}</a></li>})
+                navItem.map((item) => {
+
+                        if(item === "Filter" ){
+                            return <li key={item} onClick={filterHandle}><a href="#">{item}</a></li>    
+                        }
+                        else    
+                            return <li key={item}><a href="#">{item}</a></li>
+                    })
             }
         </nav>
     )
