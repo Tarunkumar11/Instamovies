d317fbdf1bfef0565f308856b06b8577a6e7c127
Tarun Kumar <tarun_kumar@epam.com>
2021-07-20 21:18:52 +0530

    movie - api

--- src/Components/SingleMovie/SingleMovie.js
+++ src/Components/SingleMovie/SingleMovie.js
@@ -6,24 +6,38 @@ import Cast from './Cast'
 import Recommendation from './Recommendation'
 
 function SingleMovie(props) {
-    const singleMovieData = {"backdrop_path":"/dq18nCTTLpy9PmtzZI6Y2yAgdw5.jpg","title":"Black Widow","genre_ids":[28,12,53],"original_language":"en","original_title":"Black Widow","poster_path":"/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg","video":false,"vote_average":8.2,"overview":"Natasha Romanoff, also known as Black Widow, confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by a force that will stop at nothing to bring her down, Natasha must deal with her history as a spy and the broken relationships left in her wake long before she became an Avenger.","id":497698,"vote_count":2209,"release_date":"2021-07-07","adult":false,"popularity":9790.858,"media_type":"movie"}
+    // const singleMovieData = {"backdrop_path":"/dq18nCTTLpy9PmtzZI6Y2yAgdw5.jpg","title":"Black Widow","genre_ids":[28,12,53],"original_language":"en","original_title":"Black Widow","poster_path":"/qAZ0pzat24kLdO3o8ejmbLxyOac.jpg","video":false,"vote_average":8.2,"overview":"Natasha Romanoff, also known as Black Widow, confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by a force that will stop at nothing to bring her down, Natasha must deal with her history as a spy and the broken relationships left in her wake long before she became an Avenger.","id":497698,"vote_count":2209,"release_date":"2021-07-07","adult":false,"popularity":9790.858,"media_type":"movie"}
     const playVedio = ""
-    console.log("this is a test")
-    console.log(props)
+    console.log(props.singlemoviedata.location.moviedata.moviedata)
+    const movie = props.singlemoviedata.location.moviedata.moviedata
+
+    const style  = {  
+                        backgroundImage: "url(" + "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces" + movie.backdrop_path +  ")",
+                        backgroundPosition: 'center',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat'
+                    }
+
     return (
         <div>
             <div className="single-movie-container">
-                <div className="primary-section-back">
+                {
+                    console.log("https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces"+movie.backdrop_path)
+                }
+                <div className="primary-section-back" style={style}>
                     <section className="primary-section">
                         <div className="primary-section-left">
-                            <MovieCard singleMovieData={singleMovieData}/>
+                            <MovieCard singleMovieData={movie}/>
                         </div>
                         <div className="primary-section-right">
                             <div className="content-box">
                                 <div className="movie-type">   
-                                    <h2>Movie Name</h2>
+                                    <h2>{movie.title}</h2>
                                 </div>
                                 <ul className="gener">
+                                    {/* {
+
+                                    } */}
                                     <li>tv14</li>
                                     <li>Drama</li>
                                     <li>Romance</li>
@@ -49,7 +63,7 @@ function SingleMovie(props) {
                                     <h2>Overview</h2>
                                 </div>
                                 <div className="content">
-                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias vel eaque iusto magni consequuntur, eius, blanditiis laudantium quod quasi, architecto perferendis. Doloremque minima eos similique incidunt optio deleniti vitae id?</p>
+                                    <p>{movie.overview}</p>
                                 </div>
                                 <div className="movie-type over">
                                     <h1>Tarun Saini</h1>
