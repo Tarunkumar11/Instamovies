46659a8d8598007ca23ac799143107777fade03f
Tarun Kumar <tarun_kumar@epam.com>
2021-07-21 23:54:49 +0530

    Fetch cast api and show the data on ui

--- src/Components/SingleMovie/Recommendation.js
+++ src/Components/SingleMovie/Recommendation.js
@@ -2,14 +2,11 @@ import React, {useState, useEffect} from 'react'
 import Treanding from '../Trending/Trending'
 import axios from 'axios'
 function Recommendation(props) {
-
-    console.log(props)
     const [recommendedMovie, setRecommendedMovie]   = useState(null)
     const movieId  =  props.id
     useEffect(()=> {
         const url   = `https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=01fa22077a62608ab466b3c017eba6a0&language=en-US&page=1`
         axios.get(url).then((response) => {
-            console.log(response)
             setRecommendedMovie(response.data)
             })
         }, [movieId])
